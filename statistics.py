9ba7be9df7592b8ea461a415c093c54e72253d55
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-05-10 11:04:50 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,4 +1,10 @@
-
+import math
 def calculateStats(numbers):
-  print("calculators")
-  return None
+  dict_numbers={}
+  if len(numbers)==0:
+    dict_numbers= dict.fromkeys(['avg','min','max'],math.nan)
+  else:
+    dict_numbers["avg"]=sum(numbers)/len(numbers)
+    dict_numbers["min"]=min(numbers)
+    dict_numbers["max"]=max(numbers)
+  return dict_numbers
